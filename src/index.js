38ffcfdb0b49820823671c3b5e93630491e74f27
{{Dev.Habeeb}} <olaitan6201@gmail.com>
2023-09-01 23:10:32 +0100

    moved all routes into router.js file.
    It's better to handle all routes managed in a separate file, this follows the S.O.L.I.D principle

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { RouterProvider } from "react-router-dom";
+import { UserContextProvider } from "./context/UserContext";
+import router from "./router";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <UserContextProvider>
+      <RouterProvider router={router} />
+    </UserContextProvider>
   </React.StrictMode>
 );
 
